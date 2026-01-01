818bd72efd560d57dc1a32de454df5003c0d0815
Chi Señires <chi.senires@gmail.com>
2026-01-02 02:19:05 +0800

    Remove WIP status of Archives pages

--- src/data/morePages.ts
+++ src/data/morePages.ts
@@ -11,7 +11,7 @@ export const MORE_PAGES: MorePagesEntry[] = [
   { title: "Colophon", url: "/colophon/" },
   { title: "Stuff I Use", url: "/stuff-i-use/" },
   { title: "Tags", url: "/tags/" },
-  { title: "Archive (WIP)", url: "/archive/" },
+  { title: "Archive", url: "/archive/" },
 
   // Optional: groups
   // {
