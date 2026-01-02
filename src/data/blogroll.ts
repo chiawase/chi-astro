50966f7c685f0dab7bd5ff5593dd882e83cebdce
Chi Señires <chi.senires@gmail.com>
2026-01-02 22:38:03 +0800

    Move blogroll contents into @data item and load it dynamically
    also added notes to the individual blogroll items if relevant

---
+++ src/data/blogroll.ts
@@ -0,0 +1,133 @@
+export type Blogroll = {
+  url: string;
+  domain: string;
+  author?: string;
+  note?: string;
+};
+
+export const BLOGROLL_LIST: Blogroll[] = [
+  {
+    url: "https://localghost.dev/",
+    domain: "localghost.dev",
+    author: "Sophie Koonin",
+    note: "This website is super duper cute and cool and also very creative!! I aspire for this kind of vibe",
+  },
+  {
+    url: "https://rknight.me/",
+    domain: "rknight.me",
+    author: "Robb Knight",
+    note: "One of the first few websites I got to explore when I was still exploring the fediverse and I fell in love with all the random yet relatable things here, definitely one of my inspirations for the ideal kind of personal website!",
+  },
+  {
+    url: "https://grepjason.sh/",
+    domain: "grepjason.sh",
+    author: "Jason Burk",
+    note: "Another super duper cute and cool website who I learned also had an OMG.LOL account. Also another inspiration for the kind of personal website I'd like to have! Also happy to see that I've seen a previous version of this website that isn't the same with how it's laid out now, seeing the evolution of it hehe",
+  },
+  {
+    url: "https://heydingus.net/",
+    domain: "heydingus.net",
+    author: "Jarrod Blundy",
+    note: "Another blog and personal website that serves as an inspiration for my own website! Initially got curious with his iOS Shortcuts library, and after seeing it for a while it ended up with me also getting into Shortcuts more 😁",
+  },
+  {
+    url: "https://cyrneko.eu/",
+    domain: "cyrneko.eu",
+    author: "Alexia",
+  },
+  {
+    url: "https://gerireid.com/",
+    domain: "gerireid.com",
+    author: "Geri Reid",
+  },
+  {
+    url: "https://matthewsmith.website/",
+    domain: "matthewsmith.website",
+    author: "Matthew Smith",
+  },
+  {
+    url: "https://missmansanas.com/",
+    domain: "missmansanas.com",
+    author: "Apple",
+    note: "I know Apple IRL and I'm happy to see her also writing on her own side of the internet 💖",
+  },
+  {
+    url: "https://stellify.net/",
+    domain: "stellify.net",
+    author: "Sophia Lucero",
+    note: "I know Ia IRL and super love her work! A real part of the inspiration from way back before I even knew about IndieWeb existing 😆 Also a good friend 😄",
+  },
+  {
+    url: "https://jamesiv.es/",
+    domain: "jamesiv.es",
+    author: "James Ives",
+    note: "I found his site super cool, especially with the parts of his site seemingly connected to APIs of different apps, like with Spotify data. I like those kinds of integrations so I would like to also emulate it on my personal site someday 😆",
+  },
+  {
+    url: "https://flamedfury.com/",
+    domain: "flamedfury.com",
+    author: "fLaMEd🔥",
+  },
+  {
+    url: "https://mkultra.monster/",
+    domain: "mkultra.monster",
+    author: "cmdr-nova",
+    note: "I love how very out there their website is, and it also reminded me of the old fansites that used to exist in the late 2000's and early 2010's, especially with all the anime references hahaha",
+  },
+  {
+    url: "https://rosswintle.uk/",
+    domain: "rosswintle.uk",
+    author: "Ross Wintle",
+    note: "I love the minimalist terminal-like approach to his website, another source of inspiration for me",
+  },
+  {
+    url: "https://dbushell.com/",
+    domain: "dbushell.com",
+    author: "David Bushell",
+    note: "His site is so very poster-y and magazine-y + lots of interesting posts and articles, another really great website",
+  },
+  {
+    url: "https://jamesg.blog/",
+    domain: "jamesg.blog",
+    author: "James",
+    note: "I initially saw his blog post after seeing his article on a 100 list of things to do for your website, and even the other 100 more that he listed. I still do aspire to get back to that list and do some of 'em, and I also like how many (hidden) pages he has in his website.",
+  },
+  {
+    url: "https://json.blog/",
+    domain: "json.blog",
+    author: "Jason Becker",
+  },
+  {
+    url: "https://www.thefrugalgamer.net/",
+    domain: "www.thefrugalgamer.net",
+  },
+  {
+    url: "https://www.trevorlasn.com/",
+    domain: "www.trevorlasn.com",
+    author: "Trevor I. Lasn",
+  },
+  {
+    url: "https://anhvn.com/",
+    domain: "anhvn.com",
+    author: "Anh",
+    note: "When I saw her website I found it suuuuper duper cute and another source of inspiration, particularly the games section and even the way some of her posts are styled like chat bubbles! I find that pretty neat and I resonate with that kind of way of writing haha",
+  },
+  {
+    url: "https://www.nicchan.me/",
+    domain: "www.nicchan.me",
+    author: "Nic Chan",
+    note: "Her website is styled just like a desktop!! It's super cool!! Posts show up in windows! She has a Settings modal! You can move stuff around like it's your own desktop!! Amaze ✨",
+  },
+  {
+    url: "https://www.johnpe.art/",
+    domain: "www.johnpe.art",
+    author: "John Peart",
+    note: "I stumbled upon one article of his from a newsletter I'm subbed to and found his personal website to be pretty unique. Another source of inspiration!",
+  },
+  // {
+  //   url: "",
+  //   domain: "",
+  //   author: "",
+  //   note: ""
+  // },
+];
