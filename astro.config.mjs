d1b1d083e6bce8a5132ea6342f7076818445c689
Chi Señires <chi.senires@gmail.com>
2026-01-08 17:47:08 +0800

    Add some comments to my Astro config to make it clearer which is which

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,32 @@
 import { defineConfig } from "astro/config";
 
+/* From Astro or @astrojs */
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
+/* my own data sources */
 import { SITE_URL } from "./src/consts";
 import siteRedirects from "./src/data/redirects";
 import { robotsTxtOptions } from "./src/data/robotsTxt";
 
-import embeds from "astro-embed/integration";
-import icon from "astro-icon";
-
+/* Remark Plugins */
 import wikiLinkPlugin from "@flowershow/remark-wiki-link";
 import remarkCodeTitle from "remark-code-title";
 import remarkRemoveComments from "remark-remove-comments";
 import remarkToc from "remark-toc";
 
+/* Rehype Plugins */
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeFigureTitle from "rehype-figure-title";
 
-// Personal scripts
+/* Various plugins */
+import embeds from "astro-embed/integration";
+import icon from "astro-icon";
+
+/* Personal scripts */
 import cloudinaryWatch from "./src/utils/cloudinary/watch";
 import { remarkCloudinaryLocalUploads } from "./src/utils/cloudinary/remarkLocalUploads";
 import { rehypeUploadsToCloudinary } from "./src/utils/cloudinary/rehypeUploadsToCloudinary";
